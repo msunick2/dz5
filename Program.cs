2127e0bade6c1fa42d594efad7c0ddff00d5b1ac
Nick Shipilov <msunick@mail.ru>
2023-03-19 23:03:42 +0300

    dz5

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿
+// #1
+// int [] numbers = new int [20];
+// for(int i = 0; i < numbers.Length; i++)
+// {
+//     numbers[i] = new Random().Next(100, 999);
+// }
+
+
+// void sumEvenNumbers(int [] nums)
+// {
+// int sum = 0;
+// for(int i = 0; i < nums.Length; i++)
+// {
+
+//      if(nums[i]%2 == 0)
+//     {
+//         sum++;
+//     }
+    
+// }
+// Console.Write(sum);
+// }
+// sumEvenNumbers(numbers);
+
+
+//#2
+// int [] numbers = new int [20];
+// for(int i = 0; i < 20; i++)
+// {
+//     numbers[i] = new Random().Next(1, 100);
+//    // Console.Write(numbers[i] + " ");
+// }
+
+// int sumOddPositionNumbers(int [] numbers)
+// {
+// int sum = 0;
+// for(int i = 1; i < numbers.Length; i+=2)
+// {
+//     sum += numbers[i];
+
+// }
+// return(sum);
+// }
+// Console.Write(sumOddPositionNumbers(numbers));
+
+//#3
+
+// int [] numbers = new int [20];
+// for(int i = 0; i < 20; i++)
+// {
+//     numbers[i] = new Random().Next(1, 100);
+//    // Console.Write(numbers[i] + " ");
+// }
+
+// void differenceMinMax(int[] array)
+// {
+//     int minValue = array[0];
+//     int maxValue = array[0];
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         if(minValue > array[i])
+//         {
+//             minValue = array[i];
+//         }
+//         if(maxValue < array[i])
+//         {
+//             maxValue = array[i];
+//         }
+//     }
+//     Console.Write(maxValue - minValue);
+// }
+
+// differenceMinMax(numbers);
